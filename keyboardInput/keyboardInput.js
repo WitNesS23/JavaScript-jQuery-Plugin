e9785051f320ecf006b1ef906cab154952b417b2
WitNesS23 <250357981@qq.com>
2015-09-13 18:18:33 +0800

    add callback function

--- keyboardInput/keyboardInput.js
+++ keyboardInput/keyboardInput.js
@@ -8,6 +8,7 @@
 			classStyle ： 打字效果样式情况
 			animateInterval ： 新字符出现时间间隔
 			blinkInterval ： 光标闪烁间隔
+			callback ：回调函数
 		*/
 		var defaultConfig = {
 			'container' : $(window),
@@ -15,10 +16,11 @@
 			'keyword' : '|',
 			'classStyle' : '',
 			'animateInterval' : 150,
-			'blinkInterval' : '0.3s'
+			'blinkInterval' : '0.3s',
+			'callback' : null
 		};
 
-		// 存疑： 光标颜色、宽度自适应问题
+		// 存疑： 宽度自适应问题
 
 		// 扩展过后的配置文件
 		var config = $.extend({}, defaultConfig, options || {});
@@ -29,28 +31,61 @@
 		// 保存每行输出的空DOM结构和其中需要输出的内容
 		var arrWordsContainer = [];
 
-		config.classStyle += (' white-space: nowrap; -webkit-animation : marker ' + config.blinkInterval + ' infinite;' + ' animation : marker ' + config.blinkInterval + ' infinite;');
+		// 光标闪烁CSS样式
+		var stylekeyframes = $('<style>', { 'id' : 'newStyle_Keyframes'});
+
+		stylekeyframes.html('@-webkit-keyframes marker{50% { border-color: transparent;}}@keyframes marker{50% { border-color: transparent;}}');
+
+		$('head').append(stylekeyframes);
+
+		// 包裹元素的CSS设置
+		var textColor = '';
+
+		var arrClassSet = config.classStyle.split(';');
+		var objClassSet = {};
+		for(var i = 0, length = arrClassSet.length; i < length; i++){
+			var _arr = arrClassSet[i].split(':');
+			if(Trim(_arr[0]) == '' || Trim(_arr[1]) == ''){
+				continue;
+			}else if(Trim(_arr[0]).toLowerCase() == 'color'){
+				textColor = Trim(_arr[1]);
+			}
+			config.container.css(Trim(_arr[0]), Trim(_arr[1]));
+		}
+
+		var _fontSize = config.container.css('font-size');
+
+		// 根据设置的font-size来指定闪烁光标的宽度
+		var borderWidth = 1;
+		if(_fontSize.substring(0, _fontSize.length - 2) <= 100){
+			borderWidth = _fontSize.substring(0, 1) + 'px';
+		}else{
+			borderWidth = 10 + 'px';
+		}
+
+		config.classStyle = (' white-space: nowrap; -webkit-animation : marker ' + config.blinkInterval + ' infinite;' + ' animation : marker ' + config.blinkInterval + ' infinite;');
 
 		var delayTime = 0;
 
 		for (var i = 0; i <= arrInputWords.length - 1; i++) {
-			var _WP = {};
+			var _objSignle = {};
 
 			// 不换行 注意
-			_WP.dom = $('<span>', { 'id' : 'inputWordRow_' + i, 'class' : 'inputWordRow', 'style' : config.classStyle});
-			config.container.append(_WP.dom).append($('<br />'));
+			_objSignle.dom = $('<span>', { 'id' : 'inputWordRow_' + i, 'class' : 'inputWordRow', 'style' : config.classStyle});
+			config.container.append(_objSignle.dom).append($('<br />'));
 
-			_WP.innerText = arrInputWords[i];
+			_objSignle.innerText = arrInputWords[i];
 
+			// 确定delayTime
 			if(i == 0){
-				_WP.delayTime = 0;
-				delayTime += (_WP.innerText.length + 1) * config.animateInterval;
+				_objSignle.delayTime = 0;
+				delayTime += (_objSignle.innerText.length + 1) * config.animateInterval;
 			}else{
-				_WP.delayTime = delayTime;
-				delayTime += (_WP.innerText.length + 1) * config.animateInterval;
+				_objSignle.delayTime = delayTime;
+				delayTime += (_objSignle.innerText.length + 1) * config.animateInterval;
 			}
 
-			arrWordsContainer.push(_WP);
+			arrWordsContainer.push(_objSignle);
 		}
 
 		for (var i = 0; i <= arrWordsContainer.length - 1; i++){
@@ -64,10 +99,14 @@
 			})(_this, i);
 		}
 
+		if(config.callback){
+			setTimeout(config.callback, delayTime);
+		}
+
 		function innerIntervel(i){
 			var __index = 0;
 			var _i = 2 * i + 1;
-			$(".inputWordRow:nth-child("+ _i +")").css('border-right', '1px solid black');
+			$(".inputWordRow:nth-child("+ _i +")").css('border-right', borderWidth + ' solid ' + textColor);
 			var timerId = setInterval(function(){
 				$(".inputWordRow:nth-child("+ _i +")").append(arrWordsContainer[i].innerText.charAt(__index));
 				if(++__index === arrWordsContainer[i].innerText.length){
@@ -77,6 +116,11 @@
 					}
 				}
 			}, config.animateInterval);
+		}
+
+		// 字符串去空函数
+		function Trim(str){
+			return str.replace(/(^\s*)|(\s*$)/g,'');
 		}
 	};
 })(jQuery);
